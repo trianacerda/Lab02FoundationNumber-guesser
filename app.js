4b3a8dc2ea60c0d17b67e893d186d7647fc971e2
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-06-30 14:01:24 -0700

    added basic html and started js state

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
+const userInput = document.getElementById('user-input');
+const guessButton = document.getElementById('button');
+const guessResults = document.getElementById('results');
+
 
 // initialize state
+const targetNumber = Math.floor(Math.random()*20);
+const guessesRemaining = Number(4)
+
 
 // set event listeners 
+guessButton.addEventListener ('click' () => {
+  guessesRemaining = guessesRemaining - 1;
+  
+  if (userInput === targetNumber) {
+    guessResults.textContent = `You won!`
+  } else if ()
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
