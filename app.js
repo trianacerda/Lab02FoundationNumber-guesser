ed26436afb72e2925d116fc7a3c8dfa2c1cd8808
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-06-30 16:21:59 -0700

    Working game for points, added reset button

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-// import functions and grab DOM elements
+
 const userInput = document.getElementById('user-input');
 const guessButton = document.getElementById('button');
 const guessResults = document.getElementById('results');
+const resetBtn = document.getElementById('reset');
 
-
-// initialize state
 let targetNumber = Number(Math.floor(Math.random() * 20));
 let guessesRemaining = Number(4);
 
+resetBtn.addEventListener ('click', () => {
+    location.reload();
+});
 
-// set event listeners 
 guessButton.addEventListener ('click', () => {
     guessesRemaining --;
-    console.log(targetNumber);
-    console.log(userInput.value);
-    if (userInput.value === targetNumber) {
-        guessButton.hidden = true;
+   
+    const guessedNumber = Number(userInput.value);
+    
+    if (guessedNumber === targetNumber) {
+        guessButton.disabled = true;
     }
     if (guessesRemaining === 0) {
         guessButton.disabled = true;
     }
     if (userInput.value > targetNumber) {
-        return guessResults.textContent = `Too High! Guesses Left!  ${guessesRemaining}`;
+        console.log(typeof userInput.value,'something');
+        return guessResults.textContent = `Too High! Guesses Left:  ${guessesRemaining}`;
     } else if (userInput.value < targetNumber) {
-        return guessResults.textContent = `Too Low! Guesses Left!  ${guessesRemaining}`;
+        return guessResults.textContent = `Too Low! Guesses Left:  ${guessesRemaining}`;
     } else   
-        return guessResults.textContent = `You Win! Guesses Left! ${guessesRemaining}`;
+        return guessResults.textContent = `You Win! Guesses Left: ${guessesRemaining}`;
     
     
 });
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+   
